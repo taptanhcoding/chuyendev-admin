94e0281306ddd1556434d3a0e18f606e7ed986b3
ChuyenLC <101199742+taptanhcoding@users.noreply.github.com>
2022-10-15 08:55:12 +0700

    test2

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -82,7 +82,7 @@ class CategoryController {
     Category.findOne({ slug: req.params.slug }).lean().then((category) =>
     {console.log(category);
       res.render("categorys/editCategory", {
-        category: MongooseToObject(category),
+        category
       })}
     );
   }
